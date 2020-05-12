01106f084d39b6fc385c0fd6f4638c9e3ecebe40
Edgar Zapeka <edgar.zapeka@gmail.com>
2020-05-12 20:39:35 +0200

    Add hub

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use tokio::prelude::*;
 use rchat::server::Server;
 use rchat::error::Result;
+use log::error;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() {
     env_logger::init();
 
     let server = Server::new(8080);
-    server.run().await
+    if let Err(err) = server.run().await {
+        error!("{}", err);
+    }
 }
